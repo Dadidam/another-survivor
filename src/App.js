44c6ba5f4ffda59aa0aaf67e33d2ecd78e4b2e23
Ilia <iliav@hurdit.com>
2017-11-13 14:55:18 -0500

    Fix router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Routes from "./Routes";
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import RouteNavItem from "./components/RouteNavItem";
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 
@@ -30,27 +31,37 @@ class App extends Component {
                     >
                         <div style={{ height: 32, background: "#333", borderRadius: 6, margin: 16 }} />
                         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                            <RouteNavItem key="1" href="/test">
-                                <Icon type="pie-chart" />
-                                <span>Option 1</span>
-                            </RouteNavItem>
-                            <RouteNavItem key="2" href="/ping">
-                                <Icon type="desktop" />
-                                <span>Option 2</span>
-                            </RouteNavItem>
+                            <Menu.Item key={1}>
+                                <Link to="/">
+                                    <Icon type="home" />
+                                    <span>Home</span>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key={2}>
+                                <Link to="/test">
+                                    <Icon type="pie-chart" />
+                                    <span>Option 1</span>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key={3}>
+                                <Link to="/ping">
+                                    <Icon type="desktop" />
+                                    <span>Option 2</span>
+                                </Link>
+                            </Menu.Item>
                             <SubMenu
                                 key="sub1"
                                 title={<span><Icon type="user" /><span>User</span></span>}
                             >
-                                <Menu.Item key="3">Tom</Menu.Item>
-                                <Menu.Item key="4">Bill</Menu.Item>
-                                <Menu.Item key="5">Alex</Menu.Item>
+                                <Menu.Item key="4">Tom</Menu.Item>
+                                <Menu.Item key="5">Bill</Menu.Item>
+                                <Menu.Item key="6">Alex</Menu.Item>
                             </SubMenu>
                             <SubMenu
                                 key="sub2"
                                 title={<span><Icon type="team" /><span>Team</span></span>}
                             >
-                                <Menu.Item key="6">Team 1</Menu.Item>
+                                <Menu.Item key="7">Team 1</Menu.Item>
                                 <Menu.Item key="8">Team 2</Menu.Item>
                             </SubMenu>
                             <Menu.Item key="9">
@@ -66,7 +77,7 @@ class App extends Component {
                                 <Breadcrumb.Item>Bill</Breadcrumb.Item>
                             </Breadcrumb>
                             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                                Bill is a cat.
+                                <Routes />
                             </div>
                         </Content>
                         <Footer style={{ textAlign: 'center' }}>
@@ -74,7 +85,7 @@ class App extends Component {
                         </Footer>
                     </Layout>
                 </Layout>
-                <Routes />
+                
             </div>
         );
     }
